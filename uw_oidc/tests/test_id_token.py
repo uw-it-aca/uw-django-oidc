bed82297c9cdc69158708fc1ab9382f5b060ebe4
Fang Lin <fanglin@uw.edu>
2020-03-03 15:56:59 -0800

    fix test

--- uw_oidc/tests/test_id_token.py
+++ uw_oidc/tests/test_id_token.py
@@ -5,7 +5,7 @@ from django.test import TestCase, override_settings
 from unittest.mock import patch
 from jwt.exceptions import ExpiredSignatureError
 from uw_oidc.id_token import (
-    UWIdPToken, InvalidTokenHeader, NoMatchingPublicKey,
+    UWIdPToken, InvalidTokenHeader, NoMatchingPublicKey, InvalidSignatureError,
     PyJWTError, InvalidTokenError)
 
 
@@ -21,22 +21,19 @@ class TestIdToken(TestCase):
         'nZSIsIkVtYWlsIjoiamF2ZXJhZ2VAdXcuZWR1IiwiU2NvcGVkX2FmZmlsaWF0aW9'
         'uIjoiZW1wbG95ZWUgbWVtYmVyIn0.NRlQDOWMofVPZJ5mwN5Jk_KqbV4KRAMO7rp'
         'M-rbs1tU')
-    token_wo_sig = (
+    id_token = (
         'eyJraWQiOiJkZWZhdWx0UlNBIiwiYWxnIjoiUlMyNTYifQ.eyJhdF9oYXNoIjoie'
         'mdIM2hWS0NnQU1LLS1EQWtYYi1nQSIsInN1YiI6InV3Y2RlbW8iLCJhdWQiOiJva'
         'WRjXC9teXV3IiwiYWNyIjoicGFzc3dvcmQiLCJhdXRoX3RpbWUiOjE1ODMxNzM2O'
         'DAsImlzcyI6Imh0dHBzOlwvXC9pZHAtZXZhbC51Lndhc2hpbmd0b24uZWR1IiwiZ'
         'XhwIjoxNTgzMTc3MjgxLCJpYXQiOjE1ODMxNzM2ODEsIm5vbmNlIjoiS2xuRnhwN'
-        '2JPd18wMmtubUdTblVLeC1vVE81ZnhwMEtTa2FjMk13Z05ocyJ9')
-    id_token = "{}.{}".format(
-        token_wo_sig, (
-            'T8YSGyP7LtlzbfRcWj4xs8Izeps7zyhDX12jxYGDemY3KO0v5'
-            'iSs0uAHhLddq5uS1SG53iEkMdpcXeOI6kecdye6tdGdKDpUEb'
-            'bxmpPP5VXp2eUk77YonDfWylICvWs6DKyDqE03yfopKBLrBFN2'
-            'hGk9P5ZrtvB0ZdYSd6DFgeTucNX03-g6q-q70o8o9ZDr1rz98BLdBtyA'
-            'Otwl9IJh53IioFD4U6zvS5HWjOr-7RivbwO0_BhIXS7Uo8WACY'
-            'MF6Z6VzAqfrHKixXwpvVDNyZYV2R_KqwwPVgoeT5PMM_y-xidM'
-            'MDtNlGCRDDUo0xrliuaOYrnAOzVTSDgB5cFi4Q'))
+        '2JPd18wMmtubUdTblVLeC1vVE81ZnhwMEtTa2FjMk13Z05ocyJ9.T8YSGyP7Ltlz'
+        'bfRcWj4xs8Izeps7zyhDX12jxYGDemY3KO0v5iSs0uAHhLddq5uS1SG53iEkMdpc'
+        'XeOI6kecdye6tdGdKDpUEbbxmpPP5VXp2eUk77YonDfWylICvWs6DKyDqE03yfop'
+        'KBLrBFN2hGk9P5ZrtvB0ZdYSd6DFgeTucNX03-g6q-q70o8o9ZDr1rz98BLdBtyA'
+        'Otwl9IJh53IioFD4U6zvS5HWjOr-7RivbwO0_BhIXS7Uo8WACYMF6Z6VzAqfrHKi'
+        'xXwpvVDNyZYV2R_KqwwPVgoeT5PMM_y-xidMMDtNlGCRDDUo0xrliuaOYrnAOzVT'
+        'SDgB5cFi4Q')
 
     def setUp(self):
         self.decoder = UWIdPToken()
@@ -72,17 +69,19 @@ class TestIdToken(TestCase):
         self.decoder.token = self.bad_token
         self.assertRaises(InvalidTokenError, self.decoder.validate)
 
-        # invalid signature
-        with patch.object(UWIdPToken, 'get_key') as mock1:
-            mock1.return_value = self.decoder.get_key(force_update=False)
-            self.decoder.token = self.token_wo_sig
-            self.assertRaises(InvalidTokenError, self.decoder.validate)
-            self.assertEqual(mock1.call_count, 2)
-
         with patch.object(UWIdPToken, 'get_key', return_value=None) as mock2:
             self.assertRaises(NoMatchingPublicKey, self.decoder.validate)
             self.assertEqual(mock2.call_count, 2)
 
+        # invalid signature
+        with patch.object(UWIdPToken, 'get_key') as mock1:
+            mock1.return_value = self.decoder.get_key()
+            with patch.object(UWIdPToken, 'decode_token') as mock11:
+                mock11.side_effect = InvalidSignatureError
+                self.decoder.token = self.id_token
+                self.assertRaises(InvalidTokenError, self.decoder.validate)
+                self.assertEqual(mock11.call_count, 2)
+                
     def test_valid_auth_time(self):
         # missing 'auth_time'
         self.decoder.payload = {'sub': 'javerage',
