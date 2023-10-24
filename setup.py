2c65dc1371d280f263f2aecbf867aa27adaa7576
Fang Lin <fanglin@uw.edu>
2023-10-24 09:32:19 -0700

    update django and python version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>=2.2,<3.3',
+        'Django~=4.2',
         'UW-RestClients-Core',
         'pyjwt',
         'jwcrypto'
@@ -35,6 +35,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
     ],
 )
