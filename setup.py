6df34c6fe3e904e530b9074a32b00a46ce483325
Fang Lin <fanglin@uw.edu>
2025-09-16 12:06:52 -0700

    op updates

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ setup(
     name='Uw-Django-Oidc',
     version=VERSION,
     packages=['uw_oidc'],
-    author="UW-IT T&LS",
+    author="UW-IT SETS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>=4.2,<5',
+        'Django>=4.2<6.0',
         'UW-RestClients-Core',
         'pyjwt',
         'jwcrypto'
