9db68fd4f1afe10453da37ec7273816dd04c3649
Fang Lin <fanglin@uw.edu>
2025-09-25 10:36:47 -0700

    Revise to be django 5.2–compatible

--- uw_oidc/middleware.py
+++ uw_oidc/middleware.py
@@ -6,15 +6,14 @@ from django.conf import settings
 from django.contrib import auth
 from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponse
-from django.utils.deprecation import MiddlewareMixin
 from uw_oidc.exceptions import InvalidTokenError
 from uw_oidc.id_token import UWIdPToken
-from uw_oidc.logger import log_err, log_info
+from uw_oidc.logger import log_info
 
 logger = logging.getLogger(__name__)
 
 
-class IDTokenAuthenticationMiddleware(MiddlewareMixin):
+class IDTokenAuthenticationMiddleware:
     """
     Supports ID Token (issued by UW OIDC provider)
     based request authentication for specified clients.
@@ -25,61 +24,60 @@ class IDTokenAuthenticationMiddleware(MiddlewareMixin):
     def __init__(self, get_response=None):
         self.get_response = get_response
 
-    def process_view(self, request, view_func, view_args, view_kwargs):
+    def __call__(self, request):
+        # Ensure sessions are available
         if not hasattr(request, 'session'):
             raise ImproperlyConfigured(
-                'This authentication middleware requires session middleware '
-                'to be installed. Edit your MIDDLEWARE setting to insert '
-                '"django.contrib.sessions.middleware.SessionMiddleware" '
-                'before "uw_oidc.middleware.IDTokenAuthenticationMiddleware".')
+                "This authentication middleware requires session middleware. "
+                "Insert 'django.contrib.sessions.middleware.SessionMiddleware'"
+                " before 'uw_oidc.middleware.IDTokenAuthenticationMiddleware'."
+            )
 
         if 'HTTP_AUTHORIZATION' in request.META:
             try:
-                if request.user.is_authenticated:
-                    # honor the existing session
-                    if request.user and request.session:
+                if not request.user.is_authenticated:
+                    # Conduct the authentication
+                    token = request.META['HTTP_AUTHORIZATION'].removeprefix(
+                        "Bearer ")
+
+                    username = self.clean_username(
+                        UWIdPToken().username_from_token(token))
+
+                    user = auth.authenticate(request, remote_user=username)
+                    if user:
+                        auth.login(request, user)
+
+                        # Set persistent session length in seconds
+                        request.session.set_expiry(
+                            getattr(settings, 'UW_TOKEN_SESSION_AGE', 28800))
+
+                        request.session[self.TOKEN_SESSION_KEY] = token
+                        request.session[self.USER_KEY] = username
+
                         log_info(
                             logger,
-                            {'msg': "Active session exists",
-                             'user': request.user.username,
-                             'expiry_age': request.session.get_expiry_age()})
-                    return None
+                            {'msg': "Login token-based session",
+                            'user': username,
+                            'expiry_age': request.session.get_expiry_age(),
+                            'url': request.META.get('REQUEST_URI')})
+                else:
+                    # honor existing session
+                    log_info(
+                        logger,
+                        {'msg': "Active session exists",
+                        'user': request.user.username,
+                        'expiry_age': request.session.get_expiry_age()})
 
-                # We are seeing this user for the first time in this
-                # session, attempt to authenticate the user.
-                token = request.META['HTTP_AUTHORIZATION'].replace(
-                    'Bearer ', '', 1)
-                username = self.clean_username(
-                    UWIdPToken().username_from_token(token))
-
-                user = auth.authenticate(request, remote_user=username)
-                if user:
-                    # Set logged-in user in request.user
-                    auth.login(request, user)
-
-                    # Set persistent session length in seconds
-                    request.session.set_expiry(
-                        getattr(settings, 'UW_TOKEN_SESSION_AGE', 28800))
-
-                    request.session[self.TOKEN_SESSION_KEY] = token
-                    request.session[self.USER_KEY] = username
-
-                    log_info(logger,
-                             {'msg': "Login token-based session",
-                              'user': username,
-                              'expiry_age': request.session.get_expiry_age(),
-                              'url': request.META.get('REQUEST_URI')})
             except InvalidTokenError as ex:
                 return HttpResponse(status=401, reason=str(ex))
-        return None
+        return self.get_response(request)
 
     def clean_username(self, username):
         if username is None or not len(username):
             raise InvalidTokenError('Missing username')
 
         try:
-            (username, domain) = username.split('@', 1)
+            (username, _) = username.split('@', 1)
         except ValueError:
             pass
-
         return username
